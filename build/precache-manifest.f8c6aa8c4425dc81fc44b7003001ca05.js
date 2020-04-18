4815cb0ebdeef7dd32df348f13e27b33e96a3965
Trick <petropatrickj@gmail.com>
2020-04-18 14:28:18 -0400

    updating remote

---
+++ build/precache-manifest.f8c6aa8c4425dc81fc44b7003001ca05.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df44bb819ede31e09836882a48033c72",
+    "url": "/ReactPortfolio/index.html"
+  },
+  {
+    "revision": "26de6d2fabbc5cc80833",
+    "url": "/ReactPortfolio/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9ac667ceb082c167dab2",
+    "url": "/ReactPortfolio/static/js/2.78e1a0ac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactPortfolio/static/js/2.78e1a0ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26de6d2fabbc5cc80833",
+    "url": "/ReactPortfolio/static/js/main.0f11bf74.chunk.js"
+  },
+  {
+    "revision": "2a5f3aa8c9b99516ef11",
+    "url": "/ReactPortfolio/static/js/runtime-main.e5dedc1b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactPortfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
